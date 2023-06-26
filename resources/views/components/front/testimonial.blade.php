ba3ca67be98c441f259dc4391e7fa558e0956ad9
shriradharani <brij.phpdev@gmail.com>
2023-06-26 12:31:30 +0530

    img optimization testimonials

--- resources/views/components/front/testimonial.blade.php
+++ resources/views/components/front/testimonial.blade.php
@@ -1,9 +1,28 @@
 <div class="testimonial-item bg-light rounded my-4">
     <p class="fs-5"><i class="fa fa-quote-left fa-4x text-primary mt-n4 me-3"></i>{{html_entity_decode($testimonial->testimonial_msg, ENT_NOQUOTES)}}</p>
     <div class="d-flex align-items-center">
-        <img class="img-fluid flex-shrink-0 rounded-circle"
-             src="{{ $testimonial->image ? URL::to('front/img/' . $testimonial->image) : URL::to('img/no-image.jpg') }}"
-             src="front/img/anushka-parmar.jpeg" style="width: 65px; height: 65px;">
+        
+        <?php
+        if(!empty($testimonial->image)){
+        $filepath = 'front/img/' . $testimonial->image;
+        $filename = pathinfo($filepath, PATHINFO_FILENAME); 
+        ?>
+         <picture>
+                    <source type="image/webp" srcset="front/img/{{$filename}}.webp">
+                    <source type="image/png" srcset="front/img/{{$filename}}.png">
+                    <img class="img-fluid flex-shrink-0 rounded-circle"
+             src="{{ URL::to('front/img/' . $testimonial->image) }}"
+             style="width: 65px; height: 65px;">
+                </picture>
+        <?php }else{
+            $filename = 'img/no-image.jpg';
+        ?>
+         <picture>
+                    <source type="image/webp" srcset="img/no-image.webp">
+                    <source type="image/jpeg" srcset="{{$filename}}">
+             <img class="img-fluid rounded-circle mb-4" src="{{$filename}}" alt="{{$filename}}" style="width: 64px; height: 64px">
+                </picture>
+        <?php } ?>
         <div class="ps-4">
             <h5 class="mb-1">{{$testimonial->testimonial_by}}</h5>
             @if($testimonial->rating)
