6c56f4c7c42b52c1cb60a90e9a080a84ea5db240
shriradharani <brij.phpdev@gmail.com>
2023-07-09 13:02:27 +0530

    implement of new social icon, in contact, menu active

--- resources/views/contents/front/index/contact.blade.php
+++ resources/views/contents/front/index/contact.blade.php
@@ -8,15 +8,15 @@
 <!-- About Start -->
 <div class="container-xxl py-5">
     <div class="container py-5 ">
-        
+
         <div class="row g-5 align-items-center">
             <p _class="section-title text-primary">&nbsp;<span></span></p>
-                <h1 class="mb-5">Contact Us</h1>
+            <h1 class="mb-5">Contact Us</h1>
             <div class="col-md-6 col-lg-6">
                 <p class="section-title text-info">Address<span></span></p>
                 <p class=""><i class="fa fa-map-marker-alt me-3"></i>ICET Computer Education<br/>
-                ½ C, Panchkuian Road,<br/> Nalband Crossing,<br/>
-                Near Agra College Law Faculty,<br/> Agra- 282010<br/>
+                    ½ C, Panchkuian Road,<br/> Nalband Crossing,<br/>
+                    Near Agra College Law Faculty,<br/> Agra- 282010<br/>
                 </p>
                 <hr width="50%"/>
                 <p class="mb-3 ">&nbsp;</p>
@@ -33,52 +33,33 @@
 
             <div class="col-md-6 col-lg-6">
                 <p>
-                    <div style="width: 100%">
-                        <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3549.154724057347!2d77.99819557448373!3d27.182873448430183!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3974772341cee113%3A0x2e5a5a09e43d177!2sICET%20Computer%20Education!5e0!3m2!1sen!2sin!4v1686776041824!5m2!1sen!2sin&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">area maps</a></iframe>
-                    </div>
+                <div style="width: 100%">
+                    <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3549.154724057347!2d77.99819557448373!3d27.182873448430183!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3974772341cee113%3A0x2e5a5a09e43d177!2sICET%20Computer%20Education!5e0!3m2!1sen!2sin!4v1686776041824!5m2!1sen!2sin&amp;t=p&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.maps.ie/distance-area-calculator.html">area maps</a></iframe>
+                </div>
                 </p>
             </div>
-            
+
         </div>
-        
-<!--        <div class="row g-5 align-items-center">
-            <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
-                <p class="section-title text-secondary">About Us<span></span></p>
-                <h1 class="mb-5">About Us</h1>
-                
-                <div class="skill mb-4">
+
+        <div class="row g-5 align-items-center mt-5">
+            <div class="col-lg-12 wow fadeInUp" data-wow-delay="0.1s">
+                <p class="section-title text-secondary">Follow us<span></span></p>
+                <h1 class="mb-5">Follow us on our social platforms to stay updated</h1>
+
+                <div class="social-links mb-4">
                     <div class="d-flex justify-content-between">
-                        <p class="mb-2">Thousands of video examples of language used in real life</p>
-                        <p class="mb-2">85%</p>
+                        <a class="btn btn-outline-info btn-social" target="_blank" href="https://twitter.com/icetagra"><i class="fab fa-twitter"></i> Twitter</a>
+                        <a class="btn btn-outline-primary btn-social" target="_blank" href="https://www.facebook.com/groups/617435264962620"><i class="fab fa-facebook-f"></i> Facebook</a>
+                        <a class="btn btn-outline-danger btn-social" target="_blank" href="https://www.instagram.com/arvindsutail/"><i class="fab fa-instagram"></i> Instagram</a>
+                        <a class="btn btn-outline-primary btn-social" target="_blank" href="https://www.linkedin.com/in/icet-computer-education-47b91865/"><i class="fab fa-linkedin-in"></i> LinkedIn</a>
+                        <a class="btn btn-outline-info btn-social" target="_blank" href="https://t.me/icetagr"><i class="fab fa-telegram"></i> Telegram</a>
                     </div>
-                    <div class="progress">
-                        <div class="progress-bar bg-primary" role="progressbar" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
+
                 </div>
-                <div class="skill mb-4">
-                    <div class="d-flex justify-content-between">
-                        <p class="mb-2">Immersive learning that makes you feel like you're right there with the locals</p>
-                        <p class="mb-2">90%</p>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar bg-secondary" role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-                <div class="skill mb-4">
-                    <div class="d-flex justify-content-between">
-                        <p class="mb-2">Gamified tests that train your language skills</p>
-                        <p class="mb-2">95%</p>
-                    </div>
-                    <div class="progress">
-                        <div class="progress-bar bg-dark" role="progressbar" aria-valuenow="95" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                </div>
-                <a href="" class="btn btn-primary py-sm-3 px-sm-5 rounded-pill mt-3">Read More</a>
+
             </div>
-            <div class="col-lg-6">
-                <img class="img-fluid wow zoomIn" data-wow-delay="0.5s" src="front/img/about.png">
-            </div>
-        </div>-->
+
+        </div>
     </div>
 </div>
 
